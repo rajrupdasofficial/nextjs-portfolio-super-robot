93f922f165014a8abd337a83a59e73671b7d1e1f
Rajrup Das <rajrupofficialid@protonmail.com>
2024-02-29 16:01:37 +0000

    Homepage (#2)
    * LICENSE added
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * home page design done
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    ---------
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,11 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-purple-200">
+        <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-purple-300">
           <div className="h-24">
             <NavBar />
           </div>
-          <div className="h-[calc(100vh - 6rem)]"> {children}</div>
+          <div className="h-[calc(100vh-6rem)]"> {children}</div>
         </div>
       </body>
     </html>
